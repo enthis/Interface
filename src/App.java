835b91b52b91e429cf4cf0db7dfc6b08803bccb4
harits.nurrois@pawoon.com <harits.nurrois@it.pawoon.com>
2022-12-10 15:34:38 +0700

    add code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Circle c = new Circle(12);
+        System.out.print("Luas lingkaran :"+c.area());
+        System.out.println();
+        System.out.print("Keliling lingkaran :"+c.perimeter());
+        System.out.println();
+        Rectangle r = new Rectangle(19, 19);
+        System.out.print("Luas persegi :"+r.area());
+        System.out.println();
+        System.out.print("Keliling persegi :"+r.perimeter());
+        System.out.println();
+        Triangle t = new Triangle(4, 5, 5);
+        System.out.print("Luas segitiga :"+t.area());
+        System.out.println();
+        System.out.print("Keliling segitiga :"+t.perimeter());
+        System.out.println();
+
     }
 }
